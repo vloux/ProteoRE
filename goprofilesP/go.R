bc711f539754227bc9943f7345bc76252288bbd1
NguyenLien <thi-phuong-lien.nguyen@etu.univ-amu.fr>
2017-08-14 15:58:09 +0200

    fix #16 #19 #25

--- goprofilesP/go.R
+++ goprofilesP/go.R
@@ -2,8 +2,25 @@
 library("org.Hs.eg.db", quietly=TRUE)
 library("goProfiles", quietly=TRUE)
 
-readfile = function(filename) {
-  file = read.table(filename, header = TRUE, sep = "\t")
+# Read file and return file content as data.frame?
+readfile = function(filename, header) {
+  if (header == "true") {
+    # Read only the first two lines of the files as data (without headers):
+    headers <- read.table(filename, nrows = 1, header = FALSE, sep = "\t", stringsAsFactors = FALSE, fill = TRUE)
+    #print("header")
+    #print(headers)
+    # Create the headers names with the two (or more) first rows, sappy allows to make operations over the columns (in this case paste) - read more about sapply here :
+    #headers_names <- sapply(headers, paste, collapse = "_")
+    #print(headers_names)
+    #Read the data of the files (skipping the first 2 rows):
+    file <- read.table(filename, skip = 1, header = FALSE, sep = "\t", stringsAsFactors = FALSE, fill = TRUE)
+    #print(file[1,])
+    #And assign the headers of step two to the data:
+    names(file) <- headers
+  }
+  else {
+    file <- read.table(filename, header = FALSE, sep = "\t", stringsAsFactors = FALSE, fill = TRUE)
+  }
   return(file)
 }
 
@@ -11,8 +28,8 @@ readfile = function(filename) {
 #test = readfile(filename)
 #str(test)
 #str(test$Gene.names)
-getprofile = function(filename, ncol, level) {
-
+getprofile = function(prot_ids, level) {
+  
   # Check if level is number
   if (! as.numeric(level) %% 1 == 0) {
     stop("Please enter an integer for level")
@@ -20,28 +37,45 @@ getprofile = function(filename, ncol, level) {
   else {
     level = as.numeric(level)
   }
+  #genes = as.vector(file[,ncol])
   
-  # Get file content
-  file = readfile(filename)
-  # Extract gene names (gene symbols) list
-  genes = as.factor(file[,ncol])
-
-  # Convert gene symbols into entrez ids
+  # Convert Protein IDs into entrez ids
   genes_ids = c()
-  for (gene in genes) {
-    id = as.character(mget(gene, org.Hs.egALIAS2EG, ifnotfound = NA))
-    genes_ids = append(genes_ids, id)
-  }
-
+  id = select(org.Hs.eg.db, prot_ids, "ENTREZID", "UNIPROT", multiVals = "first")
+  #print(id[[1]][1])
+  genes_ids = id$ENTREZID[which( ! is.na(id$ENTREZID))]
+  # IDs that have NA ENTREZID
+  NAs = id$UNIPROT[which(is.na(id$ENTREZID))]
+  print("IDs unable to convert to ENTREZID: ")
+  print(NAs)
+  # for (i in 1:length(id$UNIPROT)) {
+  #   print(i)
+  #   if (is.na(id[[2]][i])) {
+  #     print(id[[2]][i])
+  #   }
+  # }
+  # a = id[which(id$ENTREZID == "NA"),]
+  # print(a)
+  # print(a$UNIPROT)
+  #print(id[[1]][which(is.na(id$ENTREZID))])
+  #print(genes_ids)
+  # for (gene in genes) {
+  #   #id = as.character(mget(gene, org.Hs.egALIAS2EG, ifnotfound = NA))
+  #   id = select(org.Hs.eg.db, genes, "ENTREZID", "UNIPROT")
+  #   print(id)
+  #   genes_ids = append(genes_ids, id$ENTREZID)
+  # }
+  #print(genes_ids)
+  
   # Create basic profiles
   profile.CC = basicProfile(genes_ids, onto='CC', level=level, orgPackage="org.Hs.eg.db", empty.cats=F, ord=T, na.rm=T)
   profile.BP = basicProfile(genes_ids, onto='BP', level=level, orgPackage="org.Hs.eg.db", empty.cats=F, ord=T, na.rm=T)
   profile.MF = basicProfile(genes_ids, onto='MF', level=level, orgPackage="org.Hs.eg.db", empty.cats=F, ord=T, na.rm=T)
   profile.ALL = basicProfile(genes_ids, onto='ANY', level=level, orgPackage="org.Hs.eg.db", empty.cats=F, ord=T, na.rm=T)
-
+  
   # Print profile
   # printProfiles(profile)
-
+  
   return(c(profile.CC, profile.MF, profile.BP, profile.ALL))
 }
 
@@ -126,24 +160,38 @@ goooo = function() {
     stop("Not enough/Too many arguments", call. = FALSE)
   }
   else {
-    input = args[1]
-    ncol = args[2]
+    input_type = args[2]
+    if (input_type == "text") {
+      input = strsplit(args[1], "\\s+")[[1]]
+    }
+    else if (input_type == "file") {
+      filename = strsplit(args[1], ",")[[1]][1]
+      ncol = strsplit(args[1], ",")[[1]][2]
+      # Check ncol
+      if (! as.numeric(gsub("c", "", ncol)) %% 1 == 0) {
+        stop("Please enter an integer for level")
+      }
+      else {
+        ncol = as.numeric(gsub("c", "", ncol))
+      }
+      header = strsplit(args[1], ",")[[1]][3]
+      # Get file content
+      file = readfile(filename, header)
+      # Extract Protein IDs list
+      input = c()
+      for (row in file[,ncol]) {
+      input = c(input, strsplit(row, ";")[[1]][1])
+      }
+    }
     ontoopt = strsplit(args[3], ",")[[1]]
-    print(ontoopt)
+    #print(ontoopt)
     #plotopt = strsplit(args[3], ",")
     plotopt = args[4]
     level = args[5]
     per = as.logical(args[6])
     title = args[7]
-    
-    # Check ncol
-    if (! as.numeric(gsub("c", "", ncol)) %% 1 == 0) {
-      stop("Please enter an integer for level")
-    }
-    else {
-      ncol = as.numeric(gsub("c", "", ncol))
-    }
-    profiles = getprofile(input, ncol, level)
+
+    profiles = getprofile(input, level)
     profile.CC = profiles[1]
     #print(profile.CC)
     profile.MF = profiles[2]
@@ -187,19 +235,19 @@ goooo = function() {
       }
     }
     
-      #if (grepl("PNG", plotopt)) {
-       # plotPNG(profile.ALL = profile.ALL, per=per, title=title)
-      #}
-      #if (grepl("JPEG", plotopt)) {
-       # plotJPEG(profile.ALL = profile.ALL, per=per, title=title)
-      #}
-      #if (grepl("PDF", plotopt)) {
-       # plotPDF(profile.ALL = profile.ALL, per=per, title=title)
-      #}
+    #if (grepl("PNG", plotopt)) {
+    # plotPNG(profile.ALL = profile.ALL, per=per, title=title)
+    #}
+    #if (grepl("JPEG", plotopt)) {
+    # plotJPEG(profile.ALL = profile.ALL, per=per, title=title)
+    #}
+    #if (grepl("PDF", plotopt)) {
+    # plotPDF(profile.ALL = profile.ALL, per=per, title=title)
+    #}
   }
   
 }
 
 goooo()
 
-#Rscript go.R ../filter_kw_val/proteinGroups_Maud.txt "7" "CC" "PDF" 2 "TRUE" "Title"
+#Rscript go.R ../proteinGroups_Maud.txt "1" "CC" "PDF" 2 "TRUE" "Title"
