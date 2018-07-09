302c043c5d385b43b020adfcf49a44614164a5b4
davidchristiany <david.christiany@outlook.com>
2018-07-09 11:20:06 +0200

    KEGG_pathways_lists.sh update

--- utils/KEGG_pathways_lists.sh
+++ utils/KEGG_pathways_lists.sh
@@ -1,7 +1,8 @@
-#!/bin/bash --login                                                                                                                                                                                                                                                             
-BANK=/db/KEGG_pathways   
+#!/bin/bash --login                                                                                                                                              \
+                                                                                                                                                                  
+BANK=/db/KEGG_pathways
 DATE=`date +%d-%m-%y`
-#DATE=00-12-00
+#DATE=00-12-00                                                                                                                                                    
 
 mkdir -p $BANK/KEGG_pathways-$DATE/flat
 mkdir -p $BANK/KEGG_pathways-$DATE/merged
@@ -9,14 +10,14 @@ mkdir -p $BANK/KEGG_pathways-$DATE/info
 
 cd $BANK/KEGG_pathways-$DATE/flat
 
-#get files from kegg
+#get files from kegg                                                                                                                                              
 wget http://rest.kegg.jp/list/pathway/hsa
 wget http://rest.kegg.jp/list/pathway/mmu
 
 cd $BANK/KEGG_pathways-$DATE/merged
 
-python ./pathways_list.py -i hsa -o hsa_pathways.tsv
-python ./pathways_list.py -i mmu -o mmu_pathways.tsv
+python /usr/local/banko/VirtualEnv/biomaj-3.1.3/biomaj-3.1.3/conf/process/pathways_list.py -i $BANK/KEGG_pathways-$DATE/flat/hsa -o hsa_pathways.tsv
+python /usr/local/banko/VirtualEnv/biomaj-3.1.3/biomaj-3.1.3/conf/process/pathways_list.py -i $BANK/KEGG_pathways-$DATE/flat/mmu -o mmu_pathways.tsv
 
 cd $BANK/KEGG_pathways-$DATE/info
 ln -s /db/outils/kegg_pathways/README.txt .
@@ -27,3 +28,4 @@ rm -f current
 ln -s $BANK/KEGG_pathways-$DATE current
 
 exit 0
+
