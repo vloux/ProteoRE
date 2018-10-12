7beac20c386a8ea77b2a5a0217e28dcfd16eefa6
davidchristiany <david.christiany@outlook.com>
2018-10-12 14:31:45 +0200

    KEGG_pathways_lists.sh add rat species

--- utils/KEGG_pathways_lists.sh
+++ utils/KEGG_pathways_lists.sh
@@ -13,11 +13,13 @@ cd $BANK/KEGG_pathways-$DATE/flat
 #get files from kegg                                                                                                                                              
 wget http://rest.kegg.jp/list/pathway/hsa
 wget http://rest.kegg.jp/list/pathway/mmu
+wget http://rest.kegg.jp/list/pathway/rno
 
 cd $BANK/KEGG_pathways-$DATE/merged
 
 python /usr/local/banko/VirtualEnv/biomaj-3.1.3/biomaj-3.1.3/conf/process/pathways_list.py -i $BANK/KEGG_pathways-$DATE/flat/hsa -o hsa_pathways.tsv
 python /usr/local/banko/VirtualEnv/biomaj-3.1.3/biomaj-3.1.3/conf/process/pathways_list.py -i $BANK/KEGG_pathways-$DATE/flat/mmu -o mmu_pathways.tsv
+python /usr/local/banko/VirtualEnv/biomaj-3.1.3/biomaj-3.1.3/conf/process/pathways_list.py -i $BANK/KEGG_pathways-$DATE/flat/rno -o rno_pathways.tsv
 
 cd $BANK/KEGG_pathways-$DATE/info
 ln -s /db/outils/kegg_pathways/README.txt .
