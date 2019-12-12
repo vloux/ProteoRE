ee16d67cbeb472f5b8b9d69975bd9b0af8b7b65e
Valentin Loux <valentin.loux@jouy.inra.fr>
2019-12-12 17:06:18 +0100

    Removed old unused stuff (conda package, trainings that are now in the GTN

--- utils.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def isnumber(n):
-    numeric_format = re.compile("^[\-]?[1-9][0-9]*\.?[0-9]+$")
-    test = re.match(numeric_format, n)
-    if test:
-        return True
-    else:
-        return False
